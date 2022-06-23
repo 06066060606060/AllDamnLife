775f60b95227f6878dc7f69369a67d62e97b92b8
06066060606060 <xmicky+git@hotmail.fr>
2022-06-23 13:07:52 +0400

    ajout menu back

---
+++ resources/views/parts/compte.blade.php
@@ -0,0 +1,7 @@
+
+<div class="max-w-screen-xl mx-auto">
+<div class="flex items-center text-gray-100 pb-2">
+    <a rel="noopener noreferrer" href="/"
+        class="px-5 py-1 border-b-2 border-gray-700 hover:border-blue-400 hover:text-blue-400">Retour</a>
+</div>
+</div>
