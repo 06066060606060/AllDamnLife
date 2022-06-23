98d86e0312ff7d155bc8ecb931a7638eaa2443bf
Flo Tlm <tailame.florent@gmail.com>
2022-06-23 09:21:46 +0400

    maj page descriptif

--- resources/views/parts/onecard.blade.php
+++ resources/views/parts/onecard.blade.php
@@ -1,15 +1,15 @@
 <!-- component -->
 
-<div class="container m-auto px-6 py-20 md:px-12 lg:px-20">
+<div class="container m-auto px-6 py-4 md:px-12 lg:px-20">
     <div class="m-auto text-center lg:w-8/12 xl:w-7/12">
         <h2 class="text-2xl text-gray-300 font-bold md:text-5xl">Google Play</h2>
     </div>
     <div class="mt-12 m-auto -space-y-4 items-center justify-center md:flex md:space-y-0 md:-space-x-4 xl:w-10/12">
         <div class="relative z-10 -mx-4 group md:w-6/12 md:mx-0 lg:w-5/12">
             <div aria-hidden="true"
-                class="absolute top-0 w-full h-full rounded-2xl bg-emerald-500  shadow-xl transition duration-500 group-hover:scale-105 lg:group-hover:scale-110">
+                class=" btnmenu absolute top-0 w-full h-full rounded-2xl bg-emerald-500  shadow-xl transition duration-500 group-hover:scale-105 lg:group-hover:scale-110">
             </div>
-            <div class="relative p-6 space-y-6 lg:p-8">
+            <div class=" relative p-6 space-y-6 lg:p-8">
 
 
                 <div class="relative flex justify-around">
@@ -24,40 +24,40 @@
                                 <div class="flex flex-col items-center py-6 space-y-3">
                                     <div class="flex space-x-3">
                                         <button type="button" title="Rate 1 stars" aria-label="Rate 1 stars">
-                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-                                                class="w-10 h-10 dark:text-yellow-500">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                                fill="currentColor" class="w-10 h-10 text-yellow-500">
                                                 <path
                                                     d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
                                                 </path>
                                             </svg>
                                         </button>
                                         <button type="button" title="Rate 2 stars" aria-label="Rate 2 stars">
-                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-                                                class="w-10 h-10 dark:text-yellow-500">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                                fill="currentColor" class="w-10 h-10 text-yellow-500">
                                                 <path
                                                     d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
                                                 </path>
                                             </svg>
                                         </button>
                                         <button type="button" title="Rate 3 stars" aria-label="Rate 3 stars">
-                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-                                                class="w-10 h-10 dark:text-yellow-500">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                                fill="currentColor" class="w-10 h-10 text-yellow-500">
                                                 <path
                                                     d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
                                                 </path>
                                             </svg>
                                         </button>
                                         <button type="button" title="Rate 4 stars" aria-label="Rate 4 stars">
-                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-                                                class="w-10 h-10 dark:text-yellow-500">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                                fill="currentColor" class="w-10 h-10 text-yellow-500">
                                                 <path
                                                     d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
                                                 </path>
                                             </svg>
                                         </button>
                                         <button type="button" title="Rate 5 stars" aria-label="Rate 5 stars">
-                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-                                                class="w-10 h-10 dark:text-gray-600">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                                fill="currentColor" class="w-10 h-10 text-gray-600">
                                                 <path
                                                     d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
                                                 </path>
@@ -66,7 +66,7 @@
                                     </div>
                                 </div>
                             </div>
-                    
+
                         </div>
                     </div>
                 </div>
@@ -79,57 +79,59 @@
 
         <div class="relative group md:w-5/12 lg:w-6/12">
             <div aria-hidden="true"
-                class="absolute top-0 w-full h-full rounded-2xl bg-gray-600 shadow-lg transition duration-500 group-hover:scale-105">
+                class=" btnmenu absolute top-0 w-full h-full rounded-2xl bg-gray-600 shadow-lg transition duration-500 group-hover:scale-105">
             </div>
             <div class="relative p-6 pt-16 md:p-8 md:pl-12 md:rounded-r-2xl lg:pl-20 lg:p-16">
                 <ul role="list" class="space-y-4 py-6 text-white">
                     <!-- component -->
-<div class="flex flex-col">
-  <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
-    <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-      <div class="overflow-hidden">
-        <table class="min-w-full text-center">
-          
-          <div class="tables p-4 space-y-2 text-xl-2">
-        
-            
-           
-           <tr class="border-b bg-gray-800 boder-gray-900">
-            <td class="text-sm text-white font-medium px-6 py-4 whitespace-nowrap">
-              Prix
-            </td>
-            
-            <td class="text-sm text-white font-light px-6 py-4 whitespace-nowrap">
-              20 euros
-            </td>
-          </tr>
-      </div>
-          
-           
-            <tr class="border-b bg-gray-800 boder-gray-900">
-              <td class="text-sm text-white font-medium px-6 py-4 whitespace-nowrap">
-                Descriptif
-              </td>
-              
-              <td class="text-sm text-white font-light px-6 py-4 whitespace-nowrap">
-                doùdspj*fdĵds*ĵds*ĵds*ŝ
-              </td>
-            </tr>
-        </div>
-        </table>
-      </div>
-    </div>
-  </div>
-</div>
-                </ul>
-                <div class="bpay space-x-10 p-2">
-                                <button type="button" class="px-8 py-3 font-semibold rounded-full dark:bg-gray-100 dark:text-gray-800">Payer</button>
-                
-                                <button type="button" class="px-8 py-3 font-semibold rounded-full dark:bg-emerald-500 dark:text-gray-800">Ajouter au panier</button>
-                </div>
+                    <div class="flex flex-col">
+                        <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                            <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+                                <div class="overflow-hidden">
+                                    <table class="min-w-full text-center">
+
+                                        <div class="tables p-4 space-y-2 text-xl-2">
+
+
+
+                                            <tr class="border-b bg-gray-800 boder-gray-900">
+                                                <td class="text-sm text-white font-medium px-6 py-4 whitespace-nowrap">
+                                                    Prix
+                                                </td>
+
+                                                <td class="text-sm text-white font-light px-6 py-4 whitespace-nowrap">
+                                                    20 euros
+                                                </td>
+                                            </tr>
+                                        </div>
+
+
+                                        <tr class="border-b bg-gray-800 boder-gray-900">
+                                            <td class="text-sm text-white font-medium px-6 py-4 whitespace-nowrap">
+                                                Descriptif
+                                            </td>
+
+                                            <td class="text-sm text-white font-light px-6 py-4 whitespace-nowrap">
+                                                doùdspj*fdĵds*ĵds*ĵds*ŝ
+                                            </td>
+                                        </tr>
+                                </div>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
             </div>
-            
+            </ul>
+            <div class="bpay space-x-10 p-2">
+                <button type="button"
+                    class="px-8 py-3 font-semibold rounded-full bg-gray-100 text-gray-800">Payer</button>
+
+                <button type="button"
+                    class="px-8 py-3 font-semibold rounded-full bg-emerald-500 text-gray-800">Ajouter au
+                    panier</button>
+            </div>
         </div>
+
     </div>
 </div>
 
@@ -140,26 +142,32 @@
 
 
 
-<div class="container flex flex-col w-full max-w-lg p-6 mx-auto divide-y rounded-md divide-gray-700 dark:bg-gray-900 dark:text-gray-100">
-	<div class="flex justify-between p-4">
-		<div class="flex space-x-4">
-			<div>
-				<img src="https://source.unsplash.com/100x100/?portrait" alt="" class="object-cover w-12 h-12 rounded-full dark:bg-gray-500">
-			</div>
-			<div>
-				<h4 class="font-bold">Leroy Jenkins</h4>
-				<span class="text-xl dark:text-gray-400">2 days ago</span>
-			</div>
-		</div>
-		<div class="flex items-center space-x-2 dark:text-yellow-500">
-			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-5 h-5 fill-current">
-				<path d="M494,198.671a40.536,40.536,0,0,0-32.174-27.592L345.917,152.242,292.185,47.828a40.7,40.7,0,0,0-72.37,0L166.083,152.242,50.176,171.079a40.7,40.7,0,0,0-22.364,68.827l82.7,83.368-17.9,116.055a40.672,40.672,0,0,0,58.548,42.538L256,428.977l104.843,52.89a40.69,40.69,0,0,0,58.548-42.538l-17.9-116.055,82.7-83.368A40.538,40.538,0,0,0,494,198.671Zm-32.53,18.7L367.4,312.2l20.364,132.01a8.671,8.671,0,0,1-12.509,9.088L256,393.136,136.744,453.3a8.671,8.671,0,0,1-12.509-9.088L144.6,312.2,50.531,217.37a8.7,8.7,0,0,1,4.778-14.706L187.15,181.238,248.269,62.471a8.694,8.694,0,0,1,15.462,0L324.85,181.238l131.841,21.426A8.7,8.7,0,0,1,461.469,217.37Z"></path>
-			</svg>
-			<span class="text-xl font-bold">4.5</span>
-		</div>
-	</div>
-	<div class="p-4 space-y-2 text-xl dark:text-gray-400">
-		<p>Vivamus sit amet turpis leo. Praesent varius eleifend elit, eu dictum lectus consequat vitae. Etiam ut dolor id justo fringilla finibus.</p>
-		<p>Donec eget ultricies diam, eu molestie arcu. Etiam nec lacus eu mauris cursus venenatis. Maecenas gravida urna vitae accumsan feugiat. Vestibulum commodo, ante sit urna purus rutrum sem.</p>
-	</div>
+<div
+    class=" btnmenu container flex flex-col w-full max-w-lg p-6 mt-4 mx-auto divide-y rounded-md divide-gray-700 bg-gray-900 text-gray-100">
+    <div class="flex justify-between p-4">
+        <div class="flex space-x-4">
+            <div>
+                <img src="https://source.unsplash.com/100x100/?portrait" alt=""
+                    class="object-cover w-12 h-12 rounded-full bg-gray-500">
+            </div>
+            <div>
+                <h4 class="font-bold">Leroy Jenkins</h4>
+                <span class="text-xl text-gray-400">2 days ago</span>
+            </div>
+        </div>
+        <div class="flex items-center space-x-2 text-yellow-500">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-5 h-5 fill-current">
+                <path
+                    d="M494,198.671a40.536,40.536,0,0,0-32.174-27.592L345.917,152.242,292.185,47.828a40.7,40.7,0,0,0-72.37,0L166.083,152.242,50.176,171.079a40.7,40.7,0,0,0-22.364,68.827l82.7,83.368-17.9,116.055a40.672,40.672,0,0,0,58.548,42.538L256,428.977l104.843,52.89a40.69,40.69,0,0,0,58.548-42.538l-17.9-116.055,82.7-83.368A40.538,40.538,0,0,0,494,198.671Zm-32.53,18.7L367.4,312.2l20.364,132.01a8.671,8.671,0,0,1-12.509,9.088L256,393.136,136.744,453.3a8.671,8.671,0,0,1-12.509-9.088L144.6,312.2,50.531,217.37a8.7,8.7,0,0,1,4.778-14.706L187.15,181.238,248.269,62.471a8.694,8.694,0,0,1,15.462,0L324.85,181.238l131.841,21.426A8.7,8.7,0,0,1,461.469,217.37Z">
+                </path>
+            </svg>
+            <span class="text-xl font-bold">4.5</span>
+        </div>
+    </div>
+    <div class="p-4 space-y-2 text-xl text-gray-400">
+        <p>Vivamus sit amet turpis leo. Praesent varius eleifend elit, eu dictum lectus consequat vitae. Etiam ut dolor
+            id justo fringilla finibus.</p>
+        <p>Donec eget ultricies diam, eu molestie arcu. Etiam nec lacus eu mauris cursus venenatis. Maecenas gravida
+            urna vitae accumsan feugiat. Vestibulum commodo, ante sit urna purus rutrum sem.</p>
+    </div>
 </div>
