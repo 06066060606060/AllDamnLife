32e513e13adebf46371eb429c642a8c473730c2d
06066060606060 <xmicky+git@hotmail.fr>
2022-06-24 11:22:04 +0400

    ajout migration

--- database/migrations/2022_06_24_050925_create_user_table.php
+++ database/migrations/2022_06_24_050925_create_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,39 +13,41 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('user', function (Blueprint $table) {
-            $table->increments('id_user');
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
             $table->string('nom');
             $table->string('prenom');
-            $table->longText('email')->index();
-            $table->text('password');
-            $table->text('pinCode');
-            $table->text('confirmation_token');
-            $table->enum('confirmation_object');
-            $table->enum('gender');
-            $table->enum('etat_compte');
-            $table->dateTime('date_derniere_connexion');
-            $table->enum('profil');
-            $table->enum('type_identification');
-            $table->tinyInteger('actif');
-            $table->date('date_naissance');
-            $table->string('username');
-            $table->string('numero_telephone');
-            $table->string('adress');
-            $table->string('zipCode');
-            $table->string('city');
-            $table->string('country');
-            $table->longText('photo');
-            $table->string('last_email');
-            $table->string('remember_token');
-            $table->timestamp('created_at');
-            $table->timestamp('updated_at');
-            $table->integer('seuilId')->index();
-            $table->integer('parrainId')->index();
-            $table->enum('etat_relance');
-            $table->integer('nombre_relance');
-            $table->text('refused_token');
-            $table->date('date_last_relance');
+            $table->string('email')->unique();
+            $table->text('password')->nullable();
+            $table->text('pinCode')->nullable();
+            $table->text('confirmation_token')->nullable();
+            $table->enum('confirmation_object' , ['','SIGNUP_WAITING_EMAIL_VALIDATION','NEW_EMAIL_WAITING_VALIDATION','PASSWORD_CHANGE'])->nullable();
+            $table->enum('gender',['HOMME','FEMME','AUTRE'])->nullable();
+            $table->enum('etat_compte',['SIGNUP_SOCIAL_WAITING_IDENTITY_VALIDATION','SIGNUP_WAITING_EMAIL_VALIDATION','SIGNUP_WAITING_CGU_VALIDATION','SIGNUP_WAITING_STEP_PACK','SIGNUP_WAITING_STEP_PAYMENT','SIGNUP_WAITING_STEP_CF','SIGNUP_WAITING_STEP_PARTNER_LOGIN','SIGNUP_WAITING_STEP_IMPORT_PASSWORD','ACCOUNT_BANNED','ACCOUNT_ACTIVATED','ACCOUNT_CANCELED','ACCOUNT_DEACTIVATED','ACCOUNT_DELETED'])->default('SIGNUP_WAITING_EMAIL_VALIDATION');
+            $table->dateTime('date_derniere_connexion')->nullable();
+            $table->enum('profil', ['admin','abonne','client'])->default('abonne');;
+            $table->enum('type_identification', ['ADL','SOCIAL'])->default('ADL');;
+            $table->tinyInteger('actif')->default(0);
+            $table->date('date_naissance')->nullable();
+            $table->string('username')->nullable();
+            $table->string('numero_telephone')->nullable();
+            $table->string('address')->nullable();
+            $table->string('zipCode')->nullable();
+            $table->string('city')->nullable();
+            $table->string('country')->nullable();
+            $table->longText('photo')->nullable();
+            $table->string('last_email')->nullable();
+            $table->string('remember_token')->nullable();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->on('update')->useCurrent();
+            $table->integer('seuilId')->nullable();
+            $table->integer('parrainId')->nullable();
+            $table->enum('etat_relance', ['RELANCE','INSCRIT','REFUSE'])->nullable();
+            $table->integer('nombre_relance')->default(0);
+            $table->text('refused_token')->nullable();
+            $table->date('date_last_relance')->nullable();
+            $table->timestamp('deleted_at')->useCurrent();;
+        
         });
     }
 
