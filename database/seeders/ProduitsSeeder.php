014e6995706b280927a8fe33aee8bb2550875709
06066060606060 <xmicky+git@hotmail.fr>
2022-06-24 14:42:18 +0400

    ajout migration

---
+++ database/seeders/ProduitsSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\Produits;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProduitsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Produits::create(['titre'=> 'Carte Netflix', 'description'=> 'carte abo netflix', 'prix'=>'20',  'image'=> '/img/netflix.png', 'cat_id'=>'1', 'quantite'=>'10']);
+        Produits::create(['titre'=> 'Carte Disney+', 'description'=> 'carte abo disney', 'prix'=>'20',  'image'=> '/img/disney.png', 'cat_id'=>'1', 'quantite'=>'10']);
+        Produits::create(['titre'=> 'Carte xbox', 'description'=> 'carte abo xbox live', 'prix'=>'20',  'image'=> '/img/xbox.png', 'cat_id'=>'1', 'quantite'=>'10']);
+        Produits::create(['titre'=> 'Carte steam', 'description'=> 'carte abo steam', 'prix'=>'20',  'image'=> '/img/team.png', 'cat_id'=>'1', 'quantite'=>'10']);
+        Produits::create(['titre'=> 'Carte amazon', 'description'=> 'carte abo amazon', 'prix'=>'20',  'image'=> '/img/amazon.png', 'cat_id'=>'1', 'quantite'=>'10']);
+        Produits::create(['titre'=> 'Carte apple', 'description'=> 'carte abo itunes', 'prix'=>'20',  'image'=> '/img/itunes.png', 'cat_id'=>'1', 'quantite'=>'10']);
+        Produits::create(['titre'=> 'Carte psn+', 'description'=> 'carte abo playstation', 'prix'=>'20',  'image'=> '/img/psn.png', 'cat_id'=>'1', 'quantite'=>'10']);
+        Produits::create(['titre'=> 'Carte google', 'description'=> 'carte google play', 'prix'=>'20',  'image'=> '/img/google.png', 'cat_id'=>'1', 'quantite'=>'10']);
+    }
+}
--- resources/views/parts/section.blade.php
+++ resources/views/parts/section.blade.php
@@ -1,11 +1,7 @@
 
-{{-- SECTION FILTRES --}}
-
-
-{{-- SECTION CONTENT --}}
 <div class="max-w-screen-xl p-5 mx-auto text-gray-100">
 
-  {{-- CARTE --}}
+   @foreach ($produits as $produit)
     <div class="flip">
         <div class="front">
             <span class="ribbon cr cr-top cr-right cr-sticky cr-blue">20€</span>
@@ -18,121 +14,5 @@
             <a  href="/card" class="w-24 h-12 p-2 bg-blue-600 rounded hover:bg-blue-400 hover:text-gray-300">Voir plus</a>
         </div>
     </div>
-  {{---------}}
-   
-  {{-- CARTE --}}
-  <div class="flip">
-    <div class="front">
-        <span class="ribbon cr cr-top cr-right cr-sticky cr-blue">20€</span>
-        <img class="icon" src="/img/steam.png">
-        <p class="mt-12">Details avant carte</p>
-    </div>
-    <div class="back">
-
-        <p class="p-2">Details arriére de la carte..</p>
-        <a  href="/card" class="w-24 h-12 p-2 bg-blue-600 rounded hover:bg-blue-400 hover:text-gray-300">Voir plus</a>
-    </div>
+    @endforeach
 </div>
-{{---------}}
-
-  {{-- CARTE --}}
-  <div class="flip">
-    <div class="front">
-        <span class="ribbon">30€</span>
-        <img class="icon" src="/img/netflix.png">
-        <p class="mt-12">Details avant carte</p>
-    </div>
-    <div class="back">
-
-        <p class="p-2">Details arriére de la carte..</p>
-        <a href="/card" class="w-24 h-12 p-2 bg-blue-600 rounded hover:bg-blue-400 hover:text-gray-300">Voir plus</a>
-    </div>
-</div>
-{{---------}}
-
-  {{-- CARTE --}}
-  <div class="flip">
-    <div class="front">
-        <span class="ribbon">25€</span>
-        <img class="icon" src="/img/xbox.png">
-        <p class="mt-12">Details avant carte</p>
-    </div>
-    <div class="back">
-
-        <p class="p-2">Details arriére de la carte..</p>
-        <a href="/card" class="w-24 h-12 p-2 bg-blue-600 rounded hover:bg-blue-400 hover:text-gray-300">Voir plus</a>
-    </div>
-</div>
-{{---------}}
-
-  {{-- CARTE --}}
-  <div class="flip">
-    <div class="front">
-        <span class="ribbon">20€</span>
-        <img class="icon" src="/img/amazon.png">
-        <p class="mt-12">Details avant carte</p>
-    </div>
-    <div class="back">
-
-        <p class="p-2">Details arriére de la carte..</p>
-        <a href="/card" class="w-24 h-12 p-2 bg-blue-600 rounded hover:bg-blue-400 hover:text-gray-300">Voir plus</a>
-    </div>
-</div>
-{{---------}}
-
-  {{-- CARTE --}}
-  <div class="flip">
-    <div class="front">
-        <span class="ribbon">15€</span>
-        <img class="icon" src="/img/itunes.png">
-        <p class="mt-12">Details avant carte</p>
-    </div>
-    <div class="back">
-
-        <p class="p-2">Details arriére de la carte..</p>
-        <a  href="/card" class="w-24 h-12 p-2 bg-blue-600 rounded hover:bg-blue-400 hover:text-gray-300">Voir plus</a>
-    </div>
-</div>
-{{---------}}
-
-  {{-- CARTE --}}
-  <div class="flip">
-    <div class="front">
-        <span class="ribbon">10€</span>
-        <img class="icon" src="/img/disney.png">
-        <p class="mt-12">Details avant carte</p>
-    </div>
-    <div class="back">
-
-        <p class="p-2">Details arriére de la carte..</p>
-        <a  href="/card" class="w-24 h-12 p-2 bg-blue-600 rounded hover:bg-blue-400 hover:text-gray-300">Voir plus</a>
-    </div>
-</div>
-{{---------}}
-
-  {{-- CARTE --}}
-  
-
-
-
-
-  <div class="flip">
-    <div class="front">
-        <span class="ribbon">50€</span>
-        <img class="icon" src="/img/psn.png">
-        <p class="mt-12">Details avant carte</p>
-    </div>
-    <div class="back">
-
-        <p class="p-2">Details arriére de la carte..</p>
-        <a href="/card" class="w-24 h-12 p-2 bg-blue-600 rounded hover:bg-blue-400 hover:text-gray-300">Voir plus</a>
-    </div>
-</div>
-{{---------}}
-
-
-</div>
-
-
-
-
