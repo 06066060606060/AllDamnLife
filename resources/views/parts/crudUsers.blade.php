bc053df3edf9300ddd653df0a1642fa9ebd99c65
Flo Tlm <tailame.florent@gmail.com>
2022-06-25 14:09:18 +0400

    maj

--- resources/views/parts/crudUsers.blade.php
+++ resources/views/parts/crudUsers.blade.php
@@ -15,7 +15,7 @@
                     <tr class="flex flex-col rounded-l-lg bg-emerald-500 flex-no wrap sm:table-row ">
 
                         <th class="p-3 text-center ">Nom</th>
-                        <th class="p-3 text-center">Prenom</th>
+                        <th class="p-3 text-center">Prénom</th>
                         <th class="p-3 text-center">Email</th>
                         <th class="p-3 text-center " width="110px">Lire</th>
                         <th class="p-3 text-center" width="110px">Modifier</th>
