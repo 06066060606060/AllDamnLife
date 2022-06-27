4e7de6189d10d10353d398d24b3ea5e464c1771c
06066060606060 <xmicky+git@hotmail.fr>
2022-06-27 09:56:13 +0400

    fix

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -8,6 +8,8 @@ use App\Models\Produits;
 use Illuminate\Http\Models;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
 
 class CartController extends Controller
 {
