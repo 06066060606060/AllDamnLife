3f726d40a95d341750c8888e9bec521f2a459686
06066060606060 <xmicky+git@hotmail.fr>
2022-07-01 18:54:43 +0400

    ombres porté sur les filtres, +fix crud

--- resources/views/parts/filter.blade.php
+++ resources/views/parts/filter.blade.php
@@ -1,11 +1,11 @@
 <div class="w-full px-4 rounded-lg">
   
         <div class="grid grid-cols-2 gap-4 md:grid-cols-3 xl:grid-cols-5">
-            <input class="w-full px-4 py-3 text-sm text-white bg-gray-600 border-transparent rounded-md focus:border-gray-500 focus:ring-0" value="" placeholder="Recherche..." >
+            <input class="btnmenu w-full px-4 py-3 text-sm text-white bg-gray-600 border-transparent rounded-md focus:border-gray-500 focus:ring-0" value="" placeholder="Recherche..." >
             <form action="/filter" method="post">
                 @csrf
             <select name="categories"
-             class="w-full h-[43.99px] px-4 py-3 text-sm text-white bg-gray-600 border-transparent rounded-md focus:border-gray-500 focus:ring-0" onChange="this.form.submit()">
+             class="btnmenu w-full h-[43.99px] px-4 py-3 text-sm text-white bg-gray-600 border-transparent rounded-md focus:border-gray-500 focus:ring-0" onChange="this.form.submit()">
              <option value="">Catégories</option>
             @foreach ($categories as $categorie)
             <option value="{{ $categorie->id_cat }}">{{ $categorie->label }}</option>
@@ -15,19 +15,19 @@
           <form action="/filter" method="post">
             @csrf
             <select name="note"
-                class="w-full h-[43.99px] px-4 py-3 text-sm text-white bg-gray-600 border-transparent rounded-md focus:border-gray-500 focus:ring-0" onChange="this.form.submit()">
+                class="btnmenu w-full h-[43.99px] px-4 py-3 text-sm text-white bg-gray-600 border-transparent rounded-md focus:border-gray-500 focus:ring-0" onChange="this.form.submit()">
                 <option value="" class="">Notes</option>
-                <option value="1" class="text-yellow-500">1★</option>
-                <option value="2" class="text-yellow-500">2★★</option>
-                <option value="3" class="text-yellow-500">3★★★</option>
-                <option value="4" class="text-yellow-500">4★★★★</option>
-                <option value="5" class="text-yellow-500">5★★★★★</option>
+                <option value="1" class="text-yellow-500">1 ★</option>
+                <option value="2" class="text-yellow-500">2 ★★</option>
+                <option value="3" class="text-yellow-500">3 ★★★</option>
+                <option value="4" class="text-yellow-500">4 ★★★★</option>
+                <option value="5" class="text-yellow-500">5 ★★★★★</option>
             </select>
         </form>
             <form action="/filter" method="post">
                 @csrf
             <select name="prix"
-                class="w-full h-[43.99px] px-4 py-3 text-sm text-white bg-gray-600 border-transparent rounded-md focus:border-gray-500 focus:ring-0 "  onChange="this.form.submit()">
+                class="btnmenu w-full h-[43.99px] px-4 py-3 text-sm text-white bg-gray-600 border-transparent rounded-md focus:border-gray-500 focus:ring-0 "  onChange="this.form.submit()">
                 
                 <option value="">Gamme de prix</option>
                
@@ -39,7 +39,7 @@
            
             </select>
         </form>
-            <a href="/" class="px-4 py-3 text-sm text-white bg-gray-600 border-transparent rounded-md focus:border-gray-500 focus:ring-0">Effacer les filtres</a>
+            <a href="/" class="btnmenu px-4 py-3 text-sm text-white bg-gray-600 border-transparent rounded-md focus:border-gray-500 focus:ring-0">Effacer les filtres</a>
   
         </div>
 
