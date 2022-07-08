85d0c1887715b5c05b18d2bfce47a115a6f933e3
Benjamin-Nativel <benjaminhenri.nativel31@gmail.com>
2022-07-08 13:55:14 +0200

    fix centre filtre

--- resources/views/parts/footer.blade.php
+++ resources/views/parts/footer.blade.php
@@ -2,12 +2,15 @@
     <div class="flex flex-row items-center pr-3 space-x-4 sm:space-x-8">
             <img src="/img/logo1.png" class="w-12 h-12 " alt="">
             <a href="mailto:Contact@alldamnlife.net">Contactez-nous</a>
-            <a href="/mentions">Mentions Légales</a>
-    </div>
-   
-    <div class="flex flex-row flex-wrap space-x-4 md:space-x-8 	">
-            <a href="https://www.instagram.com/">Instagram</a>
-            <a href="https://www.facebook.com/">Facebook</a>
+            <a class="mx-auto" href="/mentions">Mentions Légales</a>
+        </div>
+        
+        <div class="flex flex-row flex-wrap space-x-4 md:space-x-8 	">
+            <div class="mx-4">
+                    <a class="mx-8 items-end" href="https://www.instagram.com/">Instagram</a>
+                    <a href="https://www.facebook.com/">Facebook</a>
+
+            </div>
     </div> 
    
 </div>
