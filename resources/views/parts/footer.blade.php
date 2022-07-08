f300a05898fa1e0485c5fbb4b2faf72aaf8a9548
FloreFabre <florefabre974@gmail.com>
2022-07-08 14:21:05 +0400

    ok

--- resources/views/parts/footer.blade.php
+++ resources/views/parts/footer.blade.php
@@ -1,11 +1,11 @@
 <div class="flex items-center justify-between max-w-screen-xl p-4 mx-auto">
-    <div class="flex flex-row items-center pr-3 space-x-4 sm:space-x-8">
+    <div class="flex flex-row items-center pr-3 space-x-2 sm:space-x-2">
             <img src="/img/logo1.png" class="w-12 h-12 " alt="">
             <a href="mailto:Contact@alldamnlife.net">Contactez-nous</a>
             <a href="/mentions">Mentions Légales</a>
     </div>
    
-    <div class="flex flex-row flex-wrap space-x-4 md:space-x-8 	">
+    <div class="flex flex-row flex-wrap space-x-4 md:space-x-2 	">
             <a href="https://www.instagram.com/alldamnlife/?hl=fr">Instagram</a>
             <a href="https://www.facebook.com/alldamnlife">Facebook</a>
     </div> 
