a04a9dc9fcb280f7ce2571273f5d3e7070a41584
Flo Tlm <tailame.florent@gmail.com>
2022-07-08 14:03:54 +0400

    again me !

--- resources/views/parts/footer.blade.php
+++ resources/views/parts/footer.blade.php
@@ -1,4 +1,4 @@
-<div class="flex items-center justify-between max-w-screen-xl p-4 mx-auto">
+<div class="flex items-center justify-between max-w-screen-xl p-4 mx-auto ">
     <div class="flex flex-row items-center pr-3 space-x-4 sm:space-x-8">
             <img src="/img/logo1.png" class="w-12 h-12 " alt="">
             <a href="mailto:Contact@alldamnlife.net">Contactez-nous</a>
